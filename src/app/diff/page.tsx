c6ada4e27d621e87512682425b27b6357744ea4b
Ravi Neupane <97901580+Binary-Beast-Byte@users.noreply.github.com>
2023-11-30 06:24:51 +0545

    chore: last commit

---
+++ src/app/diff/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+const page = () => {
+
+    
+  return (
+    <div>
+        <li key="1">last name</li>
+        <li key="1">last name</li>
+        <li key="2">hello</li>
+        <li>i am</li>
+        <li>ravi</li>
+        <li>neupane</li>
+    </div>
+  )
+}
+
+export default page
